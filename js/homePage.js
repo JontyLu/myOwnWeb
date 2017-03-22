512b8fef57fc5796090fdd16173b2eceba14e6ea
Jonty <jtl0208@outlook.com>
2017-03-23 00:45:48 +0800

    myweb

---
+++ js/homePage.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 2017/3/16.
+
+function homePageEvent(){
+    var recommendImage = document.getElementById("recommendImage").value;
+    var;
+}
+*/
+function navigatorMark(){
+    var navStyleBorder=document.getElementsByClassName("navigatorLink");
+    alert(navStyleBorder.value);
+
+}
+
